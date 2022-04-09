334e17bcca6dae06c47c37169bbd14fa01561554
Jonathan Flores <85594501+jiflon@users.noreply.github.com>
2022-04-09 11:29:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+//Ejercicios en Clase 08/04/22 Js Contador de caracteres
+
+//-----------Ejercicio 4-----------------------------------------------------------------------
+//Solicitar el nombre completo de un usuario e indicarle cuantos caracteres tiene su nombre
+
+let nombreComplet = prompt("Ingrese sun nombre completo")
+
+function nombre (a){
+    if (a){
+        console.log(a);
+    }
+}
+
+nombre (nombreComplet.length)
+
+
+let nombreCompleto = prompt("Ingrese sun nombre completo")
+
+function nombre (a){
+    if (a){
+        console.log(a);
+    }
+}
+
+nombre (nombreCompleto.length)
+
+/*
+Ejercicio 2:
+    Solicitar el nombre completo a un usuario e indicarle cuantas vocales tiene.
+*/
+//Ejercicios en Clase 08/04/22 Js Contador de vocales
+
+let nombreCompleta = prompt("Ingrese sun nombre completo");
+
+function contador (a){
+    if (count = a.match(/[aeiou]/gi).length);
+    return count;
+}
+
+contador (nombreCompleta)
+
